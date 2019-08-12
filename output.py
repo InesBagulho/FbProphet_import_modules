0fecd728a552e76c109b0cb105d8373642c2133e
InesBagulho <35599646+InesBagulho@users.noreply.github.com>
2019-08-12 16:31:39 +0100

    Update output.py

--- output.py
+++ output.py
@@ -38,7 +38,8 @@ class Output:
         print('Training data size', self.d_train.shape[0])
         print('Test data size', self.d_test.shape[0])
         self.prepare_output()
-        self.write_output()
+        
+        return self.fcst_output, self.db_type, self.model, self.pred_trans, self.df
 
     def make_prediction(self):
         
@@ -123,23 +124,5 @@ class Output:
         
             
             
-    def write_output(self):
-        
-        """ Write the output data to the DataBase
-        
-        """
-        update_output = self.fcst_output[['company_id', 'year', 'week', 'current_week', 'current_year', 'run_timestamp', 'forecast']]
 
-        # Write to db table
-        update_data = [x.tolist() for x in update_output.values]
-        
-        db = DB(db_type=self.db_type)
-        for data in update_data:
-            query = ('(IF EXISTS(SELECT * FROM ml.order_forecast WHERE company_id = {0}  AND year = {1} AND week = {2} AND current_week = {3} AND current_year = {4} ) UPDATE ml.order_forecast SET run_timestamp = {5}, forecast = {6} WHERE company_id = {0}  AND year = {1} AND week = {2} AND current_week = {3} AND current_year = {4} ELSE INSERT INTO ml.order_forecast(company_id, year, week, run_timestamp, forecast, current_week, current_year) VALUES ({0}, {1}, {2}, {5}, {6}, {3}, {4})) result_alias').format(*data)
-            spark.read.jdbc(url=db.jdbcUrl, table=query, properties=db.connectionProperties)
-
-        
-        self.model.plot(self.pred_trans)
-        plt.plot(self.df['ds'], self.df['y'], 'k.')
-        plt.show()
        
