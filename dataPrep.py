5651139173de48c6db855ed4006b20bc4a9ee1ed
InesBagulho <35599646+InesBagulho@users.noreply.github.com>
2019-09-12 14:33:07 +0100

    Update dataPrep.py

--- dataPrep.py
+++ dataPrep.py
@@ -1,27 +1,24 @@
 import pandas as pd
 import numpy as np
 import json
-from FbProphet_import_modules.db_connector import DB
+from db_connector import DB
+import pyspark
 
 class DataPrep:
     """ Takes all actions performed on Data """
 
-    def __init__(self):
+    def __init__(self, split, db_type, company):
         
-        with open('/dbfs/FileStore/tables/config.json') as f:
-            data = json.load(f)
-        
-        # All the data below is fetched from the configuration file
-        self.split = float(data["train_test_split"])
-        self.company = data["company"]
-        self.db_type = data["db_type"]
+        self.split = split
+        self.company_id = company
+        self.db_type = db_type
     
     def data_prep(self):
         
         self.data_load()
         train, test = self.train_test_split()
         
-        return train, test, self.d
+        return train, test, self.d_pandas
     
     def data_load(self):
     
@@ -31,26 +28,17 @@ class DataPrep:
         
         db = DB(db_type=self.db_type)
         
-        query = "(SELECT o.delivery_year,o.delivery_week,CAST([crm].[find_first_day_of_week](o.delivery_year, o.delivery_week) as varchar) as [ds], COUNT(o.delivery_year) as [y] FROM mb.orders o WHERE o.company_id = {0} AND o.delivery_year >= 2015 GROUP BY o.delivery_year,o.delivery_week) orders_alias"
-        
-        if self.company == 'Godtlevert':
-            self.company_id = "'09ECD4F0-AE58-4539-8E8F-9275B1859A19'"
-        elif self.company == 'Adams Matkasse':
-             self.company_id = "'8A613C15-35E4-471F-91CC-972F933331D7'"
-        elif self.company == 'Linas Matkasse':
-            self.company_id = "'8A613C15-35E4-471F-91CC-972F933331D7'"
-        elif self.company == 'Proviant':
-            self.company_id = "'E356EF97-15EE-44D4-9991-F50F1424949C'"
-        else:
-            print('Choose an existing company!')
+        query = """(SELECT delivery_year, delivery_week, ds, y
+                    FROM ml.input_forecast_orders 
+                    WHERE company_id = '{}') orders_alias"""
         
         query = query.format(self.company_id)
 
-        d = spark.read.option("numPartitions", 50).jdbc(url=db.jdbcUrl, table=query, properties=db.connectionProperties)
+        d = pyspark.read.option("numPartitions", 50).jdbc(url=db.jdbcUrl, table=query, properties=db.connectionProperties)
         d.orderBy("delivery_year", "delivery_week")
-        self.d = d.toPandas()
-        self.d['ds'] = pd.to_datetime(self.d['ds'])    
-        self.d.sort_values(by = ['ds'])
+        self.d_pandas = d.toPandas()
+        self.d_pandas['ds'] = pd.to_datetime(d_pandas['ds']) 
+
 
     def make_model_dataframe(self, d):
     
@@ -61,6 +49,7 @@ class DataPrep:
         return pd.DataFrame(data={'ds': d['ds'],
                                   'y': np.log(d['y']),
                                   'type': d['type']})
+                                  'oddweek': np.where((d['delivery_week'] % 2) == 0, 0, 1)})
 
 
     def train_test_split(self):
@@ -68,10 +57,10 @@ class DataPrep:
         """ Divides data and returns training and test data
     
         """
-        split_index = int(len(self.d) * self.split)
-        self.d['type'] = np.where(self.d.index < split_index, 'train', 'test')
+        split_index = int(len(self.d_pandas) * self.split)
+        self.d_pandas['type'] = np.where(self.d_pandas.index < split_index, 'train', 'test')
         
-        self.d_model = self.make_model_dataframe(self.d)
+        self.d_model = self.make_model_dataframe(self.d_pandas)
 
         
         return self.d_model[self.d_model['type'] == 'train'],  self.d_model[self.d_model['type'] == 'test']
